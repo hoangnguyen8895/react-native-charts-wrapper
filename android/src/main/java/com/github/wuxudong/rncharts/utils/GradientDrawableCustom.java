9f3ac693773b6f85799fe64cc15ce74868c7f4ad
HungNguyen <quang-hung.nguyen@quant-edge.com>
2020-02-13 16:18:42 +0700

    fix orientation

--- android/src/main/java/com/github/wuxudong/rncharts/utils/GradientDrawableCustom.java
+++ android/src/main/java/com/github/wuxudong/rncharts/utils/GradientDrawableCustom.java
@@ -13,54 +13,64 @@ public class GradientDrawableCustom extends PaintDrawable {
             @Override
             public Shader resize(int width, int height) {
                 float x0, x1, y0, y1;
-
-                x0 = 0f;
-                y0 = 0f;
-                x1 = 0f;
-                y1 = 0f;
-
                 switch (orientation) {
                     case "TOP_BOTTOM":
-                        x0 = 0;                 y0 = 0;
-                        x1 = x0;                y1 = height;
+                        x0 = 0;
+                        y0 = 0;
+                        x1 = x0;
+                        y1 = height;
                         break;
                     case "TR_BL":
-                        x0 = width;             y0 = 0;
-                        x1 = x0;                y1 = height;
+                        x0 = width;
+                        y0 = 0;
+                        x1 = 0;
+                        y1 = height;
                         break;
                     case "RIGHT_LEFT":
-                        x0 = 0;                 y0 = 0;
-                        x1 = x0;                y1 = height;
+                        x0 = width;
+                        y0 = 0;
+                        x1 = 0;
+                        y1 = y0;
                         break;
                     case "BR_TL":
-                        x0 = 0;                 y0 = 0;
-                        x1 = x0;                y1 = height;
+                        x0 = width;
+                        y0 = height;
+                        x1 = 0;
+                        y1 = 0;
                         break;
                     case "BOTTOM_TOP":
-                        x0 = 0;                 y0 = 0;
-                        x1 = x0;                y1 = height;
+                        x0 = 0;
+                        y0 = height;
+                        x1 = x0;
+                        y1 = 0;
                         break;
                     case "BL_TR":
-                        x0 = 0;                 y0 = 0;
-                        x1 = x0;                y1 = height;
+                        x0 = 0;
+                        y0 = height;
+                        x1 = width;
+                        y1 = 0;
                         break;
                     case "LEFT_RIGHT":
-                        x0 = 0;                 y0 = 0;
-                        x1 = x0;                y1 = height;
+                        x0 = 0;
+                        y0 = 0;
+                        x1 = width;
+                        y1 = y0;
                         break;
-                    case "TL_BR":
-                        x0 = 0;                 y0 = 0;
-                        x1 = x0;                y1 = height;
+                    default:/* TL_BR */
+                        x0 = 0;
+                        y0 = 0;
+                        x1 = width;
+                        y1 = height;
                         break;
                 }
 
                 return new LinearGradient(
-                    x0, y0, x1, y1,
-                    colors,
-                    positions,
-                    Shader.TileMode.MIRROR
+                        x0, y0, x1, y1,
+                        colors,
+                        positions,
+                        Shader.TileMode.MIRROR
                 );
-            }
+            };
         };
 
         setShape(new RectShape());
